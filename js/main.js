d44e42e1a641670e43d32c4e3369689af59b8591
heeseung <love01040701320@gmail.com>
2024-01-16 18:10:17 +0900

    서포터즈 작업 중, 배너 슬라이드 완료

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-var swiper = new Swiper(".mySwiper", {
+// $(document).ready(function(){
+
+// });
+var swiper = new Swiper(".banner", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
