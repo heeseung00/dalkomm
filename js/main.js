42f74370acab81cfd0f6a78729e8b51305c6cc76
heeseung <love01040701320@gmail.com>
2024-01-17 18:07:59 +0900

    footer 까지 레이아웃 완료.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
-// $(document).ready(function(){
-
-// });
-var swiper = new Swiper(".banner", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+$(document).ready(function () {
+  var swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    mousewheel: false,
+    keyboard: true,
+    autoplay: {
+      // 10초마다
+      delay: 10000,
+      disableOnInteraction: false,
+    },
+  });
 });
