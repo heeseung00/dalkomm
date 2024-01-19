1ae5034238ad994887577f5b8d0e964c304d7258
heeseung <love01040701320@gmail.com>
2024-01-19 18:11:01 +0900

    모든 밑줄 hover 밑 서브 메뉴 드롭다운 해결하기

--- js/main.js
+++ js/main.js
@@ -29,4 +29,33 @@ $(document).ready(function () {
       prevEl: ".new-menu-list .swiper-button .swiper-button-prev",
     },
   });
-});
+
+  // var closed = document.querySelector(".floating-wrap");
+
+  // const floatButton = document.querySelector(".close-icon");
+  // floatButton.addEventListener("click", function () {
+  //   window.self.close(closed);
+  // });
+ 
+  const body = document.querySelector("floating-wrap");
+  const modal = document.querySelector(".close-icon");
+
+  // isOpen 값에 따라 스크롤을 제어하는 함수
+  function controlScroll(isOpen) {
+    if (isOpen) {
+      body.style.overflow = "hidden";
+    } else {
+      body.style.overflow = "auto";
+    }
+  }
+
+  // 초기 모달 상태 설정
+  const isOpen = true;
+  controlScroll(isOpen);
+
+  modal.addEventListener("click", function () {
+    modal.style.display = "none";
+
+    // 모달이 닫힐 때는 스크롤을 다시 활성화
+    controlScroll(false);
+  });
