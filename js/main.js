9245a1f58eee909998970811c35718f6584960d9
heeseung <love01040701320@gmail.com>
2024-01-24 18:07:51 +0900

    aos 애니메이션 적용

--- js/main.js
+++ js/main.js
@@ -29,6 +29,8 @@ $(document).ready(function () {
       prevEl: ".new-menu-list .swiper-button .swiper-button-prev",
     },
   });
+
+  AOS.init();
 });
 
 function FloatingWrap() {
@@ -36,27 +38,14 @@ function FloatingWrap() {
   floatingWrap.style.display = "none";
 }
 
-// // 풀 드롭다운 메뉴 mouse(hover)효과
-// document.addEventListener('DOMContentLoaded', function() {
-//   const gnbTitles = document.querySelectorAll('.gnb-title');
-//   const depth2Menus = document.querySelectorAll('.depth2');
+// 마우스 오버시 색변경
+document.addEventListener("DOMContentLoaded", function () {
+  var titleContactLink = document.querySelector(".title-contact a");
 
-//   gnbTitles.forEach(title => {
-//     title.addEventListener('mouseover', function() {
-//       depth2Menus.forEach(depth2 => {
-//         depth2.style.display = 'block';
-//       });
-//       document.querySelector('.header-gnb-bg').style.display = 'block';
-//     });
-
-//     title.addEventListener('mouseout', function() {
-//       depth2Menus.forEach(depth2 => {
-//         depth2.style.display = 'none';
-//       });
-//       document.querySelector('.header-gnb-bg').style.display = 'none';
-//     });
-//   });
-// });
+  titleContactLink.addEventListener("mouseover", function () {
+    this.style.color = "#ff592e";
+  });
+});
 
 // depth2 영역 안에 있을 때 유지, 영역 밖으로 나가면 사라지는 효과 추가.
 document.addEventListener("DOMContentLoaded", function () {
@@ -93,5 +82,5 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
 // AOS 애니메이션
-// AOS.init();
