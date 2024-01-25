a0db730485408ef6ba80e92ef5aa39c0d2536e8e
heeseung <love01040701320@gmail.com>
2024-01-25 13:48:29 +0900

    Revert "완료"
    This reverts commit 0007c2a04bcf0c741041e1fdb3dbb914844997db.

--- js/main.js
+++ js/main.js
@@ -30,7 +30,6 @@ $(document).ready(function () {
     },
   });
 
-  // AOS 애니메이션
   AOS.init();
 });
 
@@ -48,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// gnb mouseover & mouseout 효과
+// depth2 영역 안에 있을 때 유지, 영역 밖으로 나가면 사라지는 효과 추가.
 document.addEventListener("DOMContentLoaded", function () {
   const gnbTitles = document.querySelectorAll(".gnb-title");
   const depth2Menus = document.querySelectorAll(".depth2");
@@ -74,10 +73,12 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // Close depth2 if the mouse leaves the depth2 area
-  depth2Menus.forEach((depth2) => {
-    depth2.addEventListener("mouseleave", function () {
-      depth2.style.display = "none";
+  headerGnbBg.addEventListener("mouseout", function (e) {
+    if (!e.relatedTarget || !e.relatedTarget.closest(".depth2")) {
+      depth2Menus.forEach((depth2) => {
+        depth2.style.display = "none";
+      });
       headerGnbBg.style.display = "none";
-    });
+    }
   });
 });
